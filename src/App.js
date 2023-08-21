75f6d29480d2d0cc38979c41edd4cf9378490699
Vineet-Singh-Chauhan <chauhansvineet@gmail.com>
2023-08-21 07:55:40 +0530

    add meta tags

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React, { useEffect ,useContext} from "react";
+import React, { useEffect, useContext } from "react";
 
-import {AuthContext} from "./contexts/AuthContext"
+import { AuthContext } from "./contexts/AuthContext";
 
 import {
   BrowserRouter,
   Routes,
   Route,
   Navigate,
-  useNavigate
+  useNavigate,
 } from "react-router-dom";
-
 
 //AOS
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 //CSS
-import './App.css';
-
-
-
+import "./App.css";
 
 //pages
 import LandingPage from "./pages/LandingPage";
-import SignupPage from './pages/Signup';
-import LoginPage from './pages/Login';
+import SignupPage from "./pages/Signup";
+import LoginPage from "./pages/Login";
 import Testpage from "./pages/Testpage";
 import Contact from "./pages/Contact";
 import Aboutuspage from "./components/pages/landingPage/Aboutuspage";
@@ -35,11 +31,7 @@ import Exams from "./components/pages/landingPage/Exams";
 import Modal from "./components/pages/landingPage/Modal";
 import TestAPI from "./components/pages/landingPage/TestAPI";
 
-
-
 function App() {
-
-
   //AOS initialisation
   useEffect(() => {
     AOS.init();
@@ -47,42 +39,41 @@ function App() {
   }, []);
 
   //context
-  const {currentUser} = useContext(AuthContext)
-  const RequireAuth = ({children})=>{
+  const { currentUser } = useContext(AuthContext);
+  const RequireAuth = ({ children }) => {
     const navigate = useNavigate();
-    return  currentUser ? children :<Navigate to="/login"/>;
-  }
+    return currentUser ? children : <Navigate to="/login" />;
+  };
 
   // sessionStorage.clear();
 
   return (
     <div className="bg-gradient-to-b from-[#001039] #0109d1 via-[#189bff10] to-[#0109d120]">
-      
       <BrowserRouter>
-        
         <Routes>
-          
-          <Route path="/" element={<LandingPage />}/>
-          <Route path="/login" element={<LoginPage/>} />
-          <Route path="/signup" element={<SignupPage/>} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
           {/* <Route path="/test" element={<Testpage/>} /> */}
-          <Route path="/test" element={<RequireAuth><Testpage/></RequireAuth>} />
-          <Route path="/Contact" element={<Contact/>} />
-          <Route path="/About" element={<Aboutuspage/>} />
-          <Route path="/engineering" element={<Engineering/>} />
-          <Route path="/job" element={<Job/>} />
-          <Route path="/skills" element={<Skills/>} />
-          <Route path="/exams" element={<Exams/>} />
-          <Route path="/modal" element={<Modal/>} />
-          <Route path="/testapi" element={<TestAPI/>} />
-          
+          <Route
+            path="/test"
+            element={
+              <RequireAuth>
+                <Testpage />
+              </RequireAuth>
+            }
+          />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/About" element={<Aboutuspage />} />
+          <Route path="/engineering" element={<Engineering />} />
+          <Route path="/job" element={<Job />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/exams" element={<Exams />} />
+          <Route path="/modal" element={<Modal />} />
+          <Route path="/testapi" element={<TestAPI />} />
         </Routes>
-      
       </BrowserRouter>
-      
-    
     </div>
-  
   );
 }
 
