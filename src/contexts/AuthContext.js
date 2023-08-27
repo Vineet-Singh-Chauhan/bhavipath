62b3c7716f8b10a902376903ea5b685649aa163c
Vineet-Singh-Chauhan <chauhansvineet@gmail.com>
2023-08-27 17:01:41 +0530

    origin back to bhavipath

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,21 +1,20 @@
-import {createContext, useEffect, useReducer} from "react";
+import { createContext, useEffect, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-    currentUser:(sessionStorage.getItem("user"))||null,
+  currentUser: sessionStorage.getItem("user") || null,
 };
 
-export const AuthContext = createContext(INITIAL_STATE)
+export const AuthContext = createContext(INITIAL_STATE);
 
-export const AuthContextProvider = ({children})=>{
-    const [state,dispatch] = useReducer(AuthReducer,INITIAL_STATE);
-    useEffect(()=>{
-        sessionStorage.setItem("user",JSON.stringify(state.currentUser))
-    },[state.currentUser])
-    return(
-        <AuthContext.Provider value={{currentUser:state.currentUser,dispatch}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
-
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+  useEffect(() => {
+    sessionStorage.setItem("user", JSON.stringify(state.currentUser));
+  }, [state.currentUser]);
+  return (
+    <AuthContext.Provider value={{ currentUser: state.currentUser, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
